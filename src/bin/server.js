ed697f1005b7e555736a7039dfe240ea59e4c4ee
Danny Cho <danny.cho@appfolio.com>
2017-08-10 11:37:57 -0700

    Update dependency (Webpack) + Remove axios

--- src/bin/server.js
+++ src/bin/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const http = require("http");
 const socketIo = require("socket.io");
-const axios = require("axios");
 
 const app = require("../app");
 
@@ -12,14 +11,16 @@ io.on("connection", socket => {
 	console.log("New client connection");
 	let interval = [];
 
+	socket.on("room", (room) => socket.join(room));
+	socket.on("leave", (room) => socket.leave(room));
+
 	socket.on("message", (message_info) => {
-		console.log("Received a message");
-		socket.broadcast.emit("message", message_info);
+		const { room } = message_info;
+		console.log("Received a message", room);
+		if(room) socket.broadcast.to(room).emit("message", message_info);
 	});
 
-	socket.on("disconnect", () => {
-		console.log("Client disconnected");
-	});
+	socket.on("disconnect", () => console.log("Client disconnected"));
 });
 
 server.listen(5000, () => console.log("Server listening in on port 5000"));
